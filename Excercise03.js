1dce8b09e02c6eddf2a538448995a53ce3fd86b9
RoyalDarshi <priyadarshiroy92@gmail.com>
2023-07-01 22:56:23 +0530

    First commit

---
+++ Excercise03.js
@@ -0,0 +1,4 @@
+var ul=document.getElementById("ul");
+ul.children[0].innerHTML=`<p style="color:green">Hello</p>`;
+ul.children[1].innerHTML=`<p style="color:yellow">Brad</p>`;
+ul.children[2].innerHTML=`<p style="color:red">Welcome</p>`;
---
+++ Exercise01.js
@@ -0,0 +1,13 @@
+
+        function print(){
+            var Name=document.getElementById("Name").value;
+            console.log(Name);
+            var Email=document.getElementById("Email").value;
+            console.log(Email);
+            var Phone=document.getElementById("Phone").value;
+            console.log(Phone);
+            var date=document.getElementById("Dates").value;
+            console.log(date);
+            var Time=document.getElementById("Times").value;
+            console.log(Time);
+        }
