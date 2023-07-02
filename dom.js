a8a1f6df743bce6d8765e9a9752f54d8252b2630
RoyalDarshi <priyadarshiroy92@gmail.com>
2023-07-02 19:11:36 +0530

    Dom Commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+var title=document.getElementById("main-header");
+console.log(title);
+title.style.borderBottom='solid 3px black';
+var item=document.getElementsByTagName("h2");
+item[0].innerHTML='<bold>Add Items</bold>';
+item[0].style.color="green";
