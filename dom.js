0cb264c7e13b93e5a053db184fb12c98ab7a244f
RoyalDarshi <priyadarshiroy92@gmail.com>
2023-07-04 00:26:00 +0530

    dom 2

--- dom.js
+++ dom.js
@@ -4,8 +4,13 @@
 // var item=document.getElementsByTagName("h2");
 // item[0].innerHTML='<bold>Add Items</bold>';
 // item[0].style.color="green";
-var items=document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor="green";
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";
+// var items=document.getElementsByClassName("list-group-item");
+// items[2].style.backgroundColor="green";
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold";
+// }
+var li=document.getElementsByTagName("li");
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor="gray";
+    li[i].style.padding="5px";
 }
