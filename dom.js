b62d3ae7bb357c2ac1af39a5d660d3c70c13ce9d
RoyalDarshi <priyadarshiroy92@gmail.com>
2023-07-04 18:40:55 +0530

    dom3

--- dom.js
+++ dom.js
@@ -14,11 +14,38 @@
 //     li[i].style.backgroundColor="gray";
 //     li[i].style.padding="5px";
 // }
-var lisec=document.querySelector(".list-group-item:nth-child(2)");
-lisec.style.backgroundColor="green";
-var lithird=document.querySelector(".list-group-item:nth-child(3)");
-lithird.style.display="none";
-var odd=document.querySelectorAll(".list-group-item:nth-child(odd)");
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor="green";
-}
+// var lisec=document.querySelector(".list-group-item:nth-child(2)");
+// lisec.style.backgroundColor="green";
+// var lithird=document.querySelector(".list-group-item:nth-child(3)");
+// lithird.style.display="none";
+// var odd=document.querySelectorAll(".list-group-item:nth-child(odd)");
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor="green";
+// }
+var form=document.querySelector("form");
+console.log(form);
+console.log(form.parentNode);
+console.log(form.lastElementChild);
+console.log(form.lastChild);
+console.log(form.firstElementChild);
+console.log(form.lastChild);
+console.log(form.firstChild);
+console.log(form.nextSibling);
+console.log(form.nextElementSibling);
+console.log(form.previousElementSibling);
+console.log(form.previousSibling);
+var div=document.createElement("div");
+div.setAttribute("title","hello");
+console.log(div);
+var text=document.createTextNode("Hello 3");
+var container=document.querySelector("header .container");
+var h1=document.querySelector(".container h1");
+div.appendChild(text);
+div.style.fontSize="30px";
+container.insertBefore(div,h1);
+var li=document.createElement("li");
+var text1=document.createTextNode("Hello 3");
+li.appendChild(text1);
+var container1=document.querySelector("ul");
+var ul=document.querySelector("li");
+container1.insertBefore(li,ul);
