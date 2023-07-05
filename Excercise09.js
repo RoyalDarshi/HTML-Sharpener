2c69bf6f041f61f02903d40299168bff18f05b51
RoyalDarshi <priyadarshiroy92@gmail.com>
2023-07-05 16:02:27 +0530

    delete from localstorage

--- Excercise09.js
+++ Excercise09.js
@@ -1,11 +1,31 @@
 let btn=document.getElementById("forms");
 btn.addEventListener("submit",addDetail);
+let ul=document.getElementById("ul");
+ul.addEventListener("click",deleteItem)
 function addDetail(e){
+    e.preventDefault();
     var obj={
         Name:document.getElementById("name").value,
         Eamil:document.getElementById("email").value,
         Mobile:document.getElementById("mobile").value
     }
     var objSerialize=JSON.stringify(obj);
-    localStorage.setItem(obj.Eamil,objSerialize);
+    var li=document.createElement("li");
+    li.appendChild(document.createTextNode(objSerialize));
+    let btn=document.createElement("button");
+    btn.innerText="Delete";
+    btn.className="Delete";
+    li.appendChild(btn);
+    ul.appendChild(li);    
+    localStorage.setItem(`${objSerialize}`,objSerialize);
+}
+
+function deleteItem(e) {
+    e.preventDefault();
+    if(e.target.classList.contains("Delete")){
+        let li=e.target.parentNode;
+        console.log(li.firstChild);
+        localStorage.removeItem(li.firstChild);
+        ul.removeChild(li);        
+    }
 }
