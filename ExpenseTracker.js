0d8f5147f569a8cb2f1b03a39b770044f6e1e0d7
RoyalDarshi <priyadarshiroy92@gmail.com>
2023-07-17 16:14:45 +0530

    Expense Tracker

---
+++ ExpenseTracker.js
@@ -0,0 +1,40 @@
+let form = document.getElementById("forms");
+form.addEventListener("submit", addDetail);
+let ul = document.getElementById("ul");
+function addDetail(e) {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let qty = document.getElementById("qty").value;
+    let price = document.getElementById("price").value;
+    let obj = {
+        "Name: ": name,
+        "Qty: ": qty,
+        "Price: ":price
+    };
+    localStorage.setItem(name, JSON.stringify(obj));
+    document.getElementById("name").value = "";
+    document.getElementById("qty").value = "";
+    document.getElementById("price").value = "";
+    let li = document.createElement("li");
+    li.innerHTML = `Product Name: ${name} ; Qty: ${qty} ; Price: ${price}   `;
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.classList = "btn btn-danger m-2";
+    deleteBtn.addEventListener("click", () => {
+        ul.removeChild(li);
+        localStorage.removeItem(name);
+    });
+    li.appendChild(deleteBtn);
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    editBtn.classList = "btn btn-warning";
+    editBtn.addEventListener("click", () => {
+        ul.removeChild(li);
+        localStorage.removeItem(name);
+        document.getElementById("name").value = name;
+        document.getElementById("qty").value = qty;
+        document.getElementById("price").value = price;
+    });
+    li.appendChild(editBtn);
+    ul.appendChild(li);
+}
